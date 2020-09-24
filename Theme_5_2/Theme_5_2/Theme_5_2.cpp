e4614fa2c2040e70cfbb2925d65a8b9e7f7e7118
helixcopter <71406955+helixcopter@users.noreply.github.com>
2020-09-24 13:49:32 +0300

    Add files via upload

---
+++ Theme_5_2/Theme_5_2/Theme_5_2.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, ac, bc, sum;
+	printf("Введите координаты точек: ");
+	scanf_s("%f%f%f", &a, &b, &c);
+	ac = abs(a - c);
+	bc = abs(b - c);
+	sum = ac + bc;
+	printf("Длина AC: %f\n", ac);
+	printf("Длина BC: %f\n", bc);
+	printf("Сумма: %f\n", sum);
+}
