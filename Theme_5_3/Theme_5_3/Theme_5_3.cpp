0689c7b8438b168844b6b60d7907738000ca4f49
helixcopter <71406955+helixcopter@users.noreply.github.com>
2020-09-24 13:50:27 +0300

    Add files via upload

---
+++ Theme_5_3/Theme_5_3/Theme_5_3.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, ac, bc, sum;
+	printf("Введите координаты точек (a,b,c): ");
+	scanf_s("%f%f%f", &a, &b, &c);
+	if ((c > b) || (c < a)) {
+		printf("Ошиюка - С находится не между А и В");
+	}
+	else {
+		ac = abs(a - c);
+		bc = abs(b - c);
+		sum = ac * bc;
+		printf("Произведение: %f\n", sum);
+	}
+}
