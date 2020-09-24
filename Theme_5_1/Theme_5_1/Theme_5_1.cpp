77dd2655a2503d6eec6097cd6775ce70ed6193a0
helixcopter <71406955+helixcopter@users.noreply.github.com>
2020-09-24 13:48:28 +0300

    Add files via upload

---
+++ Theme_5_1/Theme_5_1/Theme_5_1.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x1, y1, x2, y2, c;
+	printf("Введите координаты первой точки: ");
+	scanf_s("%f%f", &x1, &y1);
+	printf("Введите координаты второй точки: ");
+	scanf_s("%f%f", &x2, &y2);
+	c = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	printf("Расстояние между точками: %f", c);
+}
