4035357b8a60f5266708d390bb2b941a303738f2
helixcopter <71406955+helixcopter@users.noreply.github.com>
2020-10-04 00:17:46 +0300

    Add files via upload

---
+++ Theme_4_3/f/Theme_4_3.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h> 
+#include <math.h>  
+#include <locale.h> 
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float a, b, c; 
+	printf("Введите числа: ");
+	scanf_s("%f%f", &a, &b);
+	c = (a + b) / 2;
+	printf("Среднее арифметическое: %f", c);
+}
